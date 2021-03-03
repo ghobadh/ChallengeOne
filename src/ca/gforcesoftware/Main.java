a72358ec7c5fbb9b0aca04e1e5388e0699df01f5
Gavin Hashemi <Gavin.Hashemi@gforcesoftware.ca>
2021-03-02 22:11:43 -0500

    Initial commit for Person Class

--- src/ca/gforcesoftware/Main.java
+++ src/ca/gforcesoftware/Main.java
@@ -7,5 +7,9 @@ public class Main {
     public static void main(String[] args) {
         var invoice = new Invoice("My Invoice", new Date(), 123.45);
         System.out.println(invoice);
+
+
+        var person =  new Person();
+
     }
 }
